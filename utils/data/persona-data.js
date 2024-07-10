783bdb1f01874fce91076c7754ed6f2cf802a2b5
RuveydaKisla <ruveyda.kisla@indata.com.tr>
2024-07-10 22:36:45 +0300

    edit datas

--- utils/data/persona-data.js
+++ utils/data/persona-data.js
@@ -5,7 +5,7 @@ export const personalData = {
     "I am Ruveyda Kışla, a graduate of Celal Bayar University with a degree in Software Engineering. My primary focus is on developing my skills in mobile and web development. Throughout my academic and professional journey, I have actively engaged in various projects that have honed my abilities in these areas.",
   email: "ruveydakisla34@gmail.com",
   phone: "+905522207068",
-  address: "Sidre mah. 4309 sokak no:13 Merkez/ISPARTA",
+  address: "Merkez/ISPARTA",
   github: "https://github.com/ruveydakisla",
-  linkedIn:""
+  linkedIn: "https://www.linkedin.com/in/ruveydakisla/",
 };
