9c30d1c8e5d356dceb66b63806a00a2395175875
RuveydaKisla <ruveyda.kisla@indata.com.tr>
2024-07-10 21:49:20 +0300

    edit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+import { join } from "path";
+
+export const images = {
+  domains: [
+    "media.dev.to",
+    "avatars.githubusercontent.com",
+    "github-readme-stats.vercel.app",
+    "github-readme-streak-stats.herokuapp.com",
+    "stardev.io",
+    "github-profile-summary-cards.vercel.app",
+    "github-profile-trophy.vercel.app",
+  ],
+  dangerouslyAllowSVG: true,
+};
--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-import { join } from 'path'
- 
-
-export const images = {
-    domains: ['media.dev.to', 'avatars.githubusercontent.com', 'github-readme-stats.vercel.app', 'github-readme-streak-stats.herokuapp.com', 'stardev.io', 'github-profile-summary-cards.vercel.app', 'github-profile-trophy.vercel.app'],
-    dangerouslyAllowSVG: true,
-}
