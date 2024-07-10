fc1f84b5c3069205f44273b87cfcdc7ebd3760ae
RuveydaKisla <ruveyda.kisla@indata.com.tr>
2024-07-10 21:58:32 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,27 @@
 import { join } from "path";
 
-export const images = {
-  domains: [
-    "media.dev.to",
-    "avatars.githubusercontent.com",
-    "github-readme-stats.vercel.app",
-    "github-readme-streak-stats.herokuapp.com",
-    "stardev.io",
-    "github-profile-summary-cards.vercel.app",
-    "github-profile-trophy.vercel.app",
-  ],
-  dangerouslyAllowSVG: true,
+export default {
+  images: {
+    domains: [
+      "media.dev.to",
+      "avatars.githubusercontent.com",
+      "github-readme-stats.vercel.app",
+      "github-readme-streak-stats.herokuapp.com",
+      "stardev.io",
+      "github-profile-summary-cards.vercel.app",
+      "github-profile-trophy.vercel.app",
+    ],
+    dangerouslyAllowSVG: true,
+  },
+  basePath: process.env.NODE_ENV === "production" ? "/your-repo-name" : "",
+  trailingSlash: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      // Diğer sayfaları da buraya ekleyin
+    };
+  },
 };
