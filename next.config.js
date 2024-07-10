9d92c1b54f7c6e347dfe508ca61d9a9e7cfb45f0
RuveydaKisla <ruveyda.kisla@indata.com.tr>
2024-07-10 21:51:43 +0300

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,21 @@
-import { join } from "path";
+const path = require("path");
 
-export const images = {
-  domains: [
-    "media.dev.to",
-    "avatars.githubusercontent.com",
-    "github-readme-stats.vercel.app",
-    "github-readme-streak-stats.herokuapp.com",
-    "stardev.io",
-    "github-profile-summary-cards.vercel.app",
-    "github-profile-trophy.vercel.app",
-  ],
-  dangerouslyAllowSVG: true,
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "media.dev.to",
+        pathname: "**",
+      },
+    ],
+  },
 };
